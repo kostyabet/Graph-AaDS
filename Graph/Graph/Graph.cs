dad204b63bd856a1c2e00a9237c35e389202c46c
Kostya Betenya <betenya.kostya@mail.ru>
2024-11-20 12:59:51 +0300

    fet: create Graph class

--- Graph/Graph/Graph.cs
+++ Graph/Graph/Graph.cs
@@ -1,4 +1,6 @@
-﻿namespace Graph
+﻿using System.CodeDom;
+
+namespace Graph
 {
     public class Graph<T> : IGraph<T>
     {
@@ -7,44 +9,117 @@
             public Node(N name)
             {
                 this.name = name;
+                edges = new List<Node<N>>();
             }
-            public N name { get; }
-        }
-        
-        private T[] values;
+            public readonly N name;
+            public List<Node<N>> edges;
+        }        
+        private List<Node<T>> values;
         public Graph()
         {
-            values = new T[0];
+            values = new List<Node<T>>();
         }
-
+        public int Size()
+        {
+            return values.Count;
+        }
         public T Add(T name)
         {
+            if (IsNodeExist(name))
+            {
+                throw new Exception($"Node with {name} exist!");
+            }
+            values.Add(new Node<T>(name));
             return name;
         }
-
-        public bool AddEdge(T dest, T src)
-        {
-            return true;
+        private Node<T> getItem(T name) { 
+            if (!IsNodeExist(name))
+            {
+                throw new Exception($"Node {name} doesn't exist!");
+            }
+            foreach (var value in values)
+            {
+                if (string.Equals(value?.name?.ToString(), name?.ToString()))
+                {
+                    return value ?? new Node<T>(name);
+                }
+            }
+            throw new Exception($"Node {name} doesn't exist!");
         }
-
-        public bool Clear()
-        {
-            return true;   
-        }
-
         public T Remove(T name)
         {
+            if (!IsNodeExist(name))
+            {
+                throw new Exception($"Node {name} doesn't exist!");
+            }
+            var value = getItem(name);
+            values.Remove(value);
             return name;
         }
-
+        public bool IsNodeExist(T name)
+        {
+            foreach (var value in values)
+            {
+                if (string.Equals(value?.name?.ToString(), name?.ToString()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool AddEdge(T dest, T src)
+        {
+            if (!IsNodeExist(dest))
+            {
+                throw new Exception("Node {dest} doesn't exist!");
+            }
+            if (!IsNodeExist(src))
+            {
+                throw new Exception("Node {src} doesn't exist!");
+            }
+            var destanetion = getItem(dest);
+            var source = getItem(src);
+            destanetion.edges.Add(source);
+            return true;
+        }
+        public bool IsEdgeExist(T dest, T src)
+        {
+            if (!IsNodeExist(dest))
+            {
+                throw new Exception("Node {dest} doesn't exist!");
+            }
+            if (!IsNodeExist(src))
+            {
+                throw new Exception("Node {src} doesn't exist!");
+            }
+            var destanetion = getItem(dest);
+            var source = getItem(src);
+            return destanetion.edges.Contains(source);
+        }
+        public bool Clear()
+        {
+            values.Clear();
+            return true;
+        }
         public bool RemoveEdge(T dest, T src)
         {
+            if (!IsNodeExist(dest))
+            {
+                throw new Exception("Node {dest} doesn't exist!");
+            }
+            if (!IsNodeExist(src))
+            {
+                throw new Exception("Node {src} doesn't exist!");
+            }
+            var destanetion = getItem(dest);
+            var source = getItem(src);
+            destanetion.edges.Remove(source);
             return true;
         }
 
         public override string ToString()
         {
-            return "";
+            return values.ToArray().ToString() ?? "";
         }
     }
 }
--- Graph/Graph/IGraph.cs
+++ Graph/Graph/IGraph.cs
@@ -2,9 +2,12 @@
 {
     public interface IGraph<T>
     {
+        public int Size();
         public T Add(T name);
+        public bool IsNodeExist(T name);
         public T Remove(T name);
         public bool AddEdge(T dest, T src);
+        public bool IsEdgeExist(T dest, T src);
         public bool RemoveEdge(T dest, T src);
         public string ToString();
         public bool Clear();
